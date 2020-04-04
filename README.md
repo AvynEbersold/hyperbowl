0a8b17b0c1f9c6b8254b7b2ad034336bf8a815c0
Avyn Ebersold <avyn.ebersold@gmail.com>
2020-04-04 13:47:42 -0400

    Update README.md & load ads with postload.js

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-#Hyperbowl
-A literature creation and sharing app. Hyperbowl is intended to be a social media and content creation site for writers. Want to get the opinion of someone on your article? Hyperbowl is the place for that. Here's how Hyperbowl was created:
+# Hyperbowl
 
-#Server
+> A literature creation and sharing app. Hyperbowl is intended to be a social media and content creation site for writers. Want to get the opinion of someone on your article? Hyperbowl is the place for that. Here's how Hyperbowl was created:
+
+## Server
+
+> The Hyperbowl server is all contained in an app.js file. The server is in Node.js built mainly using express, bodyParser, ejs, mongoose, passport, and session.
+
+Unless the syntax determines otherwise, all declarations and Mongoose Schemas are defined at the top of the app.js file.
+All other operations are done further down in the file.
+
+## Pages & Styling
+
+> All web pages are in .ejs files in the views directory.
+
+Within the views directory, the partials directory contains the partial files. These are things like headers, footers, and sidebars.
+There is also a metadata partial that should be included in every page for SEO and description purposes.
+All of these pages are styled by one central CSS file. This file can be found by going through the file path public/css/styles.css. There is also a
+dark theme CSS file within the public/css/styles.css directory. The dark-theme.css file is only for color changes once the dark theme is toggled.
+
+## AJAX
+
+> AJAX operations are triggered and sent to the server when a user interacts with certain page elements.
+
+Most AJAX operations are in the buttons.js file. This can be found at public/js/buttons.js. This will catch events and send requests to the server. If there is
+any further operation needed, it is carried out with jQuery.
+
+## JavaScript
+
+> The operations of the .js files are as follows:
+
+buttons.js: catch events and trigger AJAX requests to the app.js file
+editor.js: is used for the text and image editor that makes posts
+postload.js: detects if the user has scrolled down to the bottom and loads more posts via AJAX if they have
+userinterface.js: controls dark theme and sidebar collapsing.
+welcomebanner.js: generates a random tip or hint and places it on a banner on the homepage
--- public/js/postload.js
+++ public/js/postload.js
@@ -1,52 +1,69 @@
 //jshint esversion: 9
 
-$(document).ready(function() {
-  let bottomMargin = 500;
-  if (window.location.pathname.toString() == "/" || window.location.pathname.toString() == "/recent" || window.location.pathname.toString() == "/popular") {
-    let sendingRequest = false;
-    setInterval(function() {
-      if (window.pageYOffset + window.innerHeight > $(document).height() - $(".footer").height() - bottomMargin && sendingRequest == false) {
-        sendingRequest = true;
-        $.ajax({
-          url: window.location.pathname.toString(),
-          type: 'POST',
-          dataType: 'json',
-          data: {
-            getCause: "SCROLL",
-            numberOfPosts: $(".post-thumbnail").length
-          },
-          success: function(data) {
-            for(let i = data.length - 1; i >= 0; i--){
-              let postHTML = "<div class='col-lg-3 col-md-6'>";
-              postHTML += '<div class="card post-thumbnail" id="' + data[i].id + '">';
-              postHTML += '<a href="/posts/' + data[i]._id + '" class="article-link-button">';
-              postHTML += '<div class="card-img-container">';
-              if(data[i].image){
-                postHTML += '<img src="' + data[i].image + `" class="card-img-top" alt="Cover photo" onError="this.src = 'https://picsum.photos/800/'">`;
-              } else {
-                postHTML += `<img src="https://picsum.photos/600/" class="card-img-top" alt="Cover photo" onError="this.src = 'https://picsum.photos/800/'">`;
-              }
-              postHTML += '</div></a>';
+$(document).ready(function () {
+	let bottomMargin = 500;
+	if (window.location.pathname.toString() == '/' || window.location.pathname.toString() == '/recent' || window.location.pathname.toString() == '/popular') {
+		let sendingRequest = false;
+		setInterval(function () {
+			if (window.pageYOffset + window.innerHeight > $(document).height() - $('.footer').height() - bottomMargin && sendingRequest == false) {
+				sendingRequest = true;
+				$.ajax({
+					url: window.location.pathname.toString(),
+					type: 'POST',
+					dataType: 'json',
+					data: {
+						getCause: 'SCROLL',
+						numberOfPosts: $('.post-thumbnail').length,
+					},
+					success: function (data) {
+						for (let i = data.length - 1; i >= 0; i--) {
+							let postHTML = "<div class='col-lg-3 col-md-6'>";
+							postHTML += '<div class="card post-thumbnail" id="' + data[i].id + '">';
+							postHTML += '<a href="/posts/' + data[i]._id + '" class="article-link-button">';
+							postHTML += '<div class="card-img-container">';
+							if (data[i].image) {
+								postHTML += '<img src="' + data[i].image + `" class="card-img-top" alt="Cover photo" onError="this.src = 'https://picsum.photos/800/'">`;
+							} else {
+								postHTML += `<img src="https://picsum.photos/600/" class="card-img-top" alt="Cover photo" onError="this.src = 'https://picsum.photos/800/'">`;
+							}
+							postHTML += '</div></a>';
 
-              postHTML += '<hr class="nomargin-line">';
-              postHTML += '<div class="card-body">';
-              postHTML += '<h5 class="card-title">' + data[i].title + '</h5>';
-              postHTML += '';
-              postHTML += '';
-              postHTML += '';
-              postHTML += '<p class="card-text">Posted By: <a href="/users/' + data[i].author + '">' + data[i].authorUsername + '</a>';
-              postHTML += ' | ' + data[i].datePublished;
-              postHTML += ' | <span class="bordered-txt"><i class="far fa-thumbs-up"></i> ' + data[i].numberOfLikes + '</span> <span class="bordered-txt"><i class="far fa-eye"></i> ' + data[i].numberOfViews + '</span></p>';
-              let previewSpan = document.createElement("span");
-              previewSpan.innerHTML = data[i].sections[0].trim();
-              postHTML += '<span>' + previewSpan.textContent.substring(0, 120) + '</span>';
-              postHTML += '<a href="/posts/' +  data[i]._id + '" class="btn article-link-button margin-top">Visit Article</a>';
-              $(".post-thumbnail-section").append(postHTML);
-            }
-            sendingRequest = false;
-          }
-        });
-      }
-    }, 50);
-  }
+							postHTML += '<hr class="nomargin-line">';
+							postHTML += '<div class="card-body">';
+							postHTML += '<h5 class="card-title">' + data[i].title + '</h5>';
+							postHTML += '';
+							postHTML += '';
+							postHTML += '';
+							postHTML += '<p class="card-text">Posted By: <a href="/users/' + data[i].author + '">' + data[i].authorUsername + '</a>';
+							postHTML += ' | ' + data[i].datePublished;
+							postHTML +=
+								' | <span class="bordered-txt"><i class="far fa-thumbs-up"></i> ' +
+								data[i].numberOfLikes +
+								'</span> <span class="bordered-txt"><i class="far fa-eye"></i> ' +
+								data[i].numberOfViews +
+								'</span></p>';
+							let previewSpan = document.createElement('span');
+							previewSpan.innerHTML = data[i].sections[0].trim();
+							postHTML += '<span>' + previewSpan.textContent.substring(0, 120) + '</span>';
+							postHTML += '<a href="/posts/' + data[i]._id + '" class="btn article-link-button margin-top">Visit Article</a>';
+							$('.post-thumbnail-section').append(postHTML);
+							if ($('.post-thumbnail').length % 16 == 0) {
+								$('.post-thumbnail-section').append(`<div class="col-12">
+                <div class="ad centered margin-bottom margin-top">
+                  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                  <!-- Dashboard Row Ad -->
+                  <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8234960759619801" data-ad-slot="2978351432" data-ad-format="auto" data-full-width-responsive="true"></ins>
+                  <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                  </script>
+                </div>
+              </div>`);
+							}
+						}
+						sendingRequest = false;
+					},
+				});
+			}
+		}, 50);
+	}
 });
